7a0e9eadb2fce369499d0a153a5f9fe961a7e85b
Harish-Prabhu-Developer <harishpraharshu@gmail.com>
2026-07-09 16:20:04 +0530

    Fix cors issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: (origin, callback) => callback(null, true),
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
     credentials: true,
